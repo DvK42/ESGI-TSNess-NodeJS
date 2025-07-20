416b459cfb00b135f836f59314aa2b5f93b6a886
FaziGIT <mathis.tsrt@gmail.com>
2025-07-21 01:44:26 +0200

    feat(user): can delete / toggle-activation and update-role any user if you're admin

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,55 +1,138 @@
-import { Request, Response, Router, json } from "express";
+import { json, Request, Response, Router } from "express";
 
 import { SessionService, UserService } from "../services";
 import { roleMiddleware, sessionMiddleware } from "../middlewares";
 import { UserRole } from "../models";
 
 export class UserController {
-  constructor(
-    public readonly userService: UserService,
-    public readonly sessionService: SessionService
-  ) {
-  }
-
-  async createUser(req: Request, res: Response) {
-    if(
-      !req.body
-      || !req.body.email
-      || !req.body.password
-      || !req.body.lastName
-      || !req.body.firstName
+    constructor(
+        public readonly userService: UserService,
+        public readonly sessionService: SessionService
     ) {
-      res.status(400).end();
-
-      return;
     }
 
-    try {
-      const user = await this.userService.createUser({
-        email: req.body.email,
-        role: UserRole.USER,
-        password: req.body.password,
-        lastName: req.body.lastName,
-        firstName: req.body.firstName
-      });
 
-      res.status(201).json(user);
-    } catch {
-      res.status(409).end();
+    async createUser(req: Request, res: Response) {
+        if (
+            !req.body
+            || !req.body.email
+            || !req.body.password
+            || !req.body.lastName
+            || !req.body.firstName
+        ) {
+            res.status(400).end();
+
+            return;
+        }
+
+        try {
+            const user = await this.userService.createUser({
+                email: req.body.email,
+                role: UserRole.USER,
+                password: req.body.password,
+                lastName: req.body.lastName,
+                firstName: req.body.firstName
+            });
+
+            res.status(201).json(user);
+        } catch {
+            res.status(409).end();
+        }
     }
-  }
 
-  buildRouter(): Router {
-    const router = Router();
+    async deleteUser(req: Request, res: Response) {
+        const { id } = req.params;
 
-    router.post(
-      '/',
-      sessionMiddleware(this.sessionService),
-      roleMiddleware(UserRole.ADMIN),
-      json(),
-      this.createUser.bind(this)
-    );
-    
-    return router;
-  }
+        if (!id) {
+            res.status(400).json({ error: "User ID is required" });
+            return;
+        }
+
+        try {
+            await this.userService.deleteUser(id);
+            res.status(204).end();
+        } catch (error) {
+            console.error('Error deleting user:', error);
+            res.status(404).json({ error: "User not found" });
+        }
+    }
+
+    async toggleUserActivation(req: Request, res: Response) {
+        const { id } = req.params;
+
+        if (!id) {
+            res.status(400).json({ error: "User ID is required" });
+            return;
+        }
+
+        try {
+            await this.userService.toggleUserActivation(id);
+            res.status(204).end();
+        } catch (error) {
+            res.status(404).json({ error: "User not found" });
+        }
+    }
+
+    async updateRole(req: Request, res: Response) {
+        if (!req.body || !req.body.role) {
+            res.status(400).json({ error: "Role is required" });
+            return;
+        }
+
+        if (!Object.values(UserRole).includes(req.body.role)) {
+            res.status(400).json({ error: "Invalid role, valid roles are: " + Object.values(UserRole).join(", ") });
+            return;
+        }
+
+        const { id } = req.params;
+
+        if (!id) {
+            res.status(400).json({ error: "User ID is required" });
+            return;
+        }
+
+        try {
+            await this.userService.updateRole(id, req.body.role);
+            res.status(204).end();
+        } catch (error) {
+            res.status(404).json({ error: "User not found" });
+        }
+    }
+
+    buildRouter(): Router {
+        const router = Router();
+
+        router.post(
+            '/',
+            sessionMiddleware(this.sessionService),
+            roleMiddleware(UserRole.ADMIN),
+            json(),
+            this.createUser.bind(this)
+        );
+
+        router.put(
+            '/:id/toggle-activation',
+            sessionMiddleware(this.sessionService),
+            roleMiddleware(UserRole.ADMIN),
+            json(),
+            this.toggleUserActivation.bind(this)
+        );
+
+        router.put(
+            '/:id/update-role',
+            sessionMiddleware(this.sessionService),
+            roleMiddleware(UserRole.ADMIN),
+            json(),
+            this.updateRole.bind(this)
+        );
+
+        router.delete(
+            '/:id',
+            sessionMiddleware(this.sessionService),
+            roleMiddleware(UserRole.ADMIN),
+            this.deleteUser.bind(this)
+        );
+
+        return router;
+    }
 }
--- services/userService.ts
+++ services/userService.ts
@@ -1,41 +1,68 @@
-import { Mongoose, Model, FilterQuery, isValidObjectId } from "mongoose";
+import {FilterQuery, isValidObjectId, Model, Mongoose} from "mongoose";
 
-import { User, UserRole } from "../models";
-import { userSchema } from "../mongoose";
-import { sha256 } from "../utils/security";
+import {User, UserRole} from "../models";
+import {userSchema} from "../mongoose";
+import {sha256} from "../utils/security";
 
 export type CreateUser = Omit<User, '_id' | 'createdAt' | 'updatedAt' | 'isActive'>;
 
 export class UserService {
-  readonly model: Model<User>;
+    readonly model: Model<User>;
 
-  constructor(public readonly connection: Mongoose) {
-    this.model = connection.model('User', userSchema);
-  }
-
-  async findUser(email: string, password?: string): Promise<User | null> {
-    const filter: FilterQuery<User> = { email: email };
-
-    if(password) {
-        filter.password = sha256(password);
+    constructor(public readonly connection: Mongoose) {
+        this.model = connection.model('User', userSchema);
     }
 
-    return this.model.findOne(filter);
-  }
+    async findUser(email: string, password?: string): Promise<User | null> {
+        const filter: FilterQuery<User> = {email: email};
 
-  async createUser(user: CreateUser): Promise<User> {
-      return this.model.create({...user, password: sha256(user.password)});
-  }
+        if (password) {
+            filter.password = sha256(password);
+        }
 
-  async updateRole(userId: string, role: UserRole): Promise<void> {
-      if(!isValidObjectId(userId)) {
-          return;
-      }
+        return this.model.findOne(filter);
+    }
 
-      await this.model.updateOne({
-          _id: userId
-      }, {
-          role: role
-      });
-  }
+    async createUser(user: CreateUser): Promise<User> {
+        return this.model.create({...user, password: sha256(user.password)});
+    }
+
+
+    async deleteUser(userId: string): Promise<void> {
+        if (!isValidObjectId(userId)) {
+            return;
+        }
+
+        await this.model.deleteOne({_id: userId});
+    }
+
+    async updateRole(userId: string, role: UserRole): Promise<void> {
+        if (!isValidObjectId(userId)) {
+            return;
+        }
+
+        await this.model.updateOne({
+            _id: userId
+        }, {
+            role: role
+        });
+    }
+
+    async toggleUserActivation(userId: string): Promise<void> {
+        if (!isValidObjectId(userId)) {
+            return;
+        }
+
+        // D'abord récupérer l'utilisateur pour connaître son état actuel
+        const user = await this.model.findById(userId);
+        if (!user) {
+            return;
+        }
+
+        // Inverser l'état d'activation
+        await this.model.updateOne(
+            {_id: userId},
+            {$set: {isActive: !user.isActive}}
+        );
+    }
 }
